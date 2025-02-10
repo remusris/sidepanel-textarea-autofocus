19efc1d10c12438cc75f60b6fdc209ae1f7cf516
Remus <91991249+remusris@users.noreply.github.com>
2025-02-09 21:32:35 -0700

    some config notes

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, defineRunnerConfig } from 'wxt';
 import tailwindcss from 'tailwindcss';
 
 // See https://wxt.dev/api/config.html
@@ -7,5 +7,15 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
     permissions: ["sidePanel"]
-  }  
+  },
+  dev: {
+    server: {
+      port: 5173,
+    }
+  },
+  // change this to "false" if you want a chrome window to auto-open
+  // otherwise you'll have to load the extension manually via the .output folder at the root of the project which can be hidden (windows does not hide it but macOS does[need command + shift + . to show the hidden .output folder]) 
+  runner: defineRunnerConfig({
+    disabled: true
+  }),
 });
